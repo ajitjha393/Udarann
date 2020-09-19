526c981ae3f7013774cf5e8e996af432e52e8c69
Bishwajit Jha <ajitjha393@gmail.com>
2020-09-19 16:21:13 +0530

    Rendered all the quotes in a grid list

---
+++ src/components/QuoteGrid.vue
@@ -0,0 +1,20 @@
+<template>
+	<div class="row">
+		<udarann-quote v-for="q in quotes" :key="Math.random() + q">
+			{{ q }}
+		</udarann-quote>
+	</div>
+</template>
+
+<script>
+import Quote from './Quote.vue'
+
+export default {
+	props: ['quotes'],
+	components: {
+		'udarann-quote': Quote
+	}
+}
+</script>
+
+<style scoped></style>
