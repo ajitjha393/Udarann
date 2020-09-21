d1ae2f9b2926d8fb07b34aa62d917405186026d3
Bishwajit Jha <ajitjha393@gmail.com>
2020-09-21 09:34:46 +0530

    Listening to newQuote Added event in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,32 @@
 <template>
 	<div class="container">
+		<udarann-new-quote @quoteAdded="newQuote"></udarann-new-quote>
 		<udarann-quote-grid :quotes="quotes"></udarann-quote-grid>
 	</div>
 </template>
 
 <script>
 import QuoteGrid from './components/QuoteGrid.vue'
+import NewQuote from './components/NewQuote.vue'
 
 // Quote Limit 10
 
 export default {
-	data: function() {
+	data: function () {
 		return {
 			maxQuotes: 10,
-			quotes: ['Here is a lovely Quote !']
+			quotes: ['Here is a lovely Quote !'],
 		}
 	},
+	methods: {
+		newQuote(quote) {
+			this.quotes.push(quote)
+		},
+	},
 	components: {
-		'udarann-quote-grid': QuoteGrid
-	}
+		'udarann-quote-grid': QuoteGrid,
+		'udarann-new-quote': NewQuote,
+	},
 }
 </script>
 
